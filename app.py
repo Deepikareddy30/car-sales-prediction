deb8bc791ef11cc1240bc312ca290de07d79913d
deepikareddy30 <deepikareddym3010@atlassian.com>
2023-11-22 11:54:59 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template,jsonify,request
+import pandas as pd
+import pickle
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict',methods = ['GET','POST'])
+def predict():
+    if request.method == "POST":
+        make = request.form.get("make")
+        model = request.form.get("model")
+        year = request.form.get("year")
+        fuel = request.form.get("fuel")
+        hp = request.form.get("hp")
+        cylinders = request.form.get("cylinders")
+        transmission = request.form.get("transmission")
+        wheels = request.form.get("wheels")
+        doors = request.form.get("doors")
+        size = request.form.get("size")
+        style = request.form.get("style")
+        highway = request.form.get("highway")
+        city= request.form.get("city")
+        popularity = request.form.get("popularity")
+        df = pd.read_json("new.json")
+        make_encode = df['Make_encode'][df['Make']==make].values[0]
+        model_encode = df['Model_encode'][df['Model']==model].values[0]
+        enginefueltype_encode = df['Engine Fuel Type_encode'][df['Engine Fuel Type']==fuel].values[0]
+        driven_wheels_encode = df['Driven_Wheels_encode'][df['Driven_Wheels']==wheels].values[0]
+        transmissiontype_encode = df['Transmission Type_encode'][df['Transmission Type']==transmission].values[0]
+        vehiclestyle_encode = df['Vehicle Style_encode'][df['Vehicle Style']==style].values[0]
+        vehiclesize_encode = df['Vehicle Size_encode'][df['Vehicle Size']==size].values[0]
+        print(make_encode,model_encode,enginefueltype_encode,driven_wheels_encode,transmissiontype_encode,vehiclestyle_encode,vehiclesize_encode,)
+        with open('model.pkl','rb') as model:
+            mlmodel = pickle.load(model)
+            predict = mlmodel.predict([[int(year),float(hp),float(cylinders),float(doors),float(highway),float(city),float(popularity),make_encode,model_encode,enginefueltype_encode,transmissiontype_encode,driven_wheels_encode,vehiclesize_encode,vehiclestyle_encode]])
+        print(predict)
+        
+        print(make,model,year,fuel,hp,cylinders,transmission,wheels,doors,size,style,highway,city,popularity)
+        return jsonify({"Predicted Result":f"result :{predict}"})
+    else:
+        return render_template('predict.html')
+
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0')
